511e6b77888ebd48432391fa3919dc6801fd6c6d
Ihor Debelyi <klerk333@gmail.com>
2017-11-09 15:26:43 +0000

    Formatted and second reverse function is added

--- assignment.pl
+++ assignment.pl
@@ -5,12 +5,15 @@ isElementInList(El, [_|T]):-
   isElementInList(El, T).
 
 %Definitions for reverseList(List, ReversedList)
-%reverseList(List, ReversedList):-
-%  helperReverseList(List, ReversedList, []).
-%helperReverseList([], AdditionalList, AdditionalList).
-%helperReverseList([H|T], ReversedList, AdditionalList):-
-%  helperReverseList(T, ReversedList, [H|AdditionalList]).
 
+% 1 solution
+reverseList1(List, ReversedList):-
+  helperReverseList(List, ReversedList, []).
+helperReverseList([], AdditionalList, AdditionalList).
+helperReverseList([H|T], ReversedList, AdditionalList):-
+  helperReverseList(T, ReversedList, [H|AdditionalList]).
+
+% 2 solution
 reverseList([], []).
 reverseList([H|T], ReversedList):-
   reverseList(T, List),
@@ -21,6 +24,7 @@ insertElementIntoListEnd(El, [], [El]).
 insertElementIntoListEnd(El, [H|T], NewList):-
   insertElementIntoListEnd(El, T, List),
   NewList = [H|List].
+
 %Definitions for mergeLists(List1, List2, Merged)
 mergeLists([], List2, List2).
 mergeLists([H|T], List2, Merged):-
