949627b05720b77f7250e8711ff64279f40b56fe
Ihor Debelyi <klerk333@gmail.com>
2017-11-09 15:22:56 +0000

    Question 4 is done

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,28 @@
 %Definitions for isElementInList(El, List)
 
+isElementInList(El, [El|_]).
+isElementInList(El, [_|T]):-
+  isElementInList(El, T).
+
 %Definitions for reverseList(List, ReversedList)
+%reverseList(List, ReversedList):-
+%  helperReverseList(List, ReversedList, []).
+%helperReverseList([], AdditionalList, AdditionalList).
+%helperReverseList([H|T], ReversedList, AdditionalList):-
+%  helperReverseList(T, ReversedList, [H|AdditionalList]).
+
+reverseList([], []).
+reverseList([H|T], ReversedList):-
+  reverseList(T, List),
+  mergeLists(List, [H], ReversedList).
 
 %insertElementIntoListEnd(El, List, NewList)
-
+insertElementIntoListEnd(El, [], [El]).
+insertElementIntoListEnd(El, [H|T], NewList):-
+  insertElementIntoListEnd(El, T, List),
+  NewList = [H|List].
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], List2, List2).
+mergeLists([H|T], List2, Merged):-
+  mergeLists(T, List2, L),
+  Merged = [H|L].
